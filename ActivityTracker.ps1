893f76d85985536123410641ac23c136123d1549
Armağan Savaş <asavas@gmail.com>
2024-03-20 20:36:10 +0300

    Update1

--- ActivityTracker.ps1
+++ ActivityTracker.ps1
@@ -1,8 +1,92 @@
+$NewClassName = 'Win32_UserActivityTime'
+$Date = Get-Date
+# Settings
+$activeTime = 0
+$inactiveTime = 0
+$interval = 60  # Kontrol aralığı (saniye)
+$totalTime = 0
+$targetDuration = 3600 #saniye  (1 saat) Toplam çalışacak süre hedef süre (3600 saniye üzerinden)
+$passiveThreshold = 30  # 30 saniye - #$passiveThreshold, kullanıcının bilgisayarda klavye - Mouse basmadığı süre için tolerans değeridir.Örnek: 30sn telefonda konuşabilir.O anı pasif olarak saymayacaktır. Eğer 30'den sonra halen pasif ise pasif olarak sayacaktır.
+
+
+
+
+# WMI sınıfını oluşturun
+$newClass = New-Object System.Management.ManagementClass ("root\cimv2", [String]::Empty, $null)
+$newClass["__CLASS"] = $NewClassName
+
+$newClass.Qualifiers.Add("Static", $true)
+$newClass.Properties.Add("Timestamp", [System.Management.CimType]::String, $false)
+$newClass.Properties.Add("ActiveTime", [System.Management.CimType]::String, $false)
+$newClass.Properties.Add("InactiveTime", [System.Management.CimType]::String, $false)
+$newClass.Properties.Add("ScriptLastRan", [System.Management.CimType]::String, $false)
+$newClass.Properties["Timestamp"].Qualifiers.Add("Key", $true)
+$newClass.Put() 
+
+function Write-Log() {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()]
+        [Alias("LogContent")]
+        [string]$Message,
+        [Parameter(Mandatory=$false)]
+        [Alias('LogPath')]
+        [string]$Path = "C:\Windows\Temp\Win32_UserActivityTime.Log",
+        [Parameter(Mandatory=$false)]
+        [ValidateSet("Error","Warn","Info")]
+        [string]$Level = "Info"
+    )
+    Begin {
+        # Set VerbosePreference to Continue so that verbose messages are displayed.
+        $VerbosePreference = 'Continue'
+    }
+    Process {
+		if (Test-Path $Path) {
+			$LogSize = (Get-Item -Path $Path).Length/1MB
+			$MaxLogSize = 5
+		}
+        # Check for file size of the log. If greater than 5MB, it will create a new one and delete the old.
+        if ((Test-Path $Path) -AND $LogSize -gt $MaxLogSize) {
+            Write-Error "Log file $Path already exists and file exceeds maximum file size. Deleting the log and starting fresh."
+            Remove-Item $Path -Force
+            $NewLogFile = New-Item $Path -Force -ItemType File
+        }
+        # If attempting to write to a log file in a folder/path that doesn't exist create the file including the path.
+        elseif (-NOT(Test-Path $Path)) {
+            Write-Verbose "Creating $Path."
+            $NewLogFile = New-Item $Path -Force -ItemType File
+        }
+        else {
+            # Nothing to see here yet.
+        }
+        # Format Date for our Log File
+        $FormattedDate = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+        # Write message to error, warning, or verbose pipeline and specify $LevelText
+        switch ($Level) {
+            'Error' {
+                Write-Error $Message
+                $LevelText = 'ERROR:'
+            }
+            'Warn' {
+                Write-Warning $Message
+                $LevelText = 'WARNING:'
+            }
+            'Info' {
+                Write-Verbose $Message
+                $LevelText = 'INFO:'
+            }
+        }
+        # Write log entry to $Path
+        "$FormattedDate $LevelText $Message" | Out-File -FilePath $Path -Append
+    }
+    End {
+    }
+}
+
 Add-Type @"
     using System;
-    using System.IO;
     using System.Runtime.InteropServices;
-    using System.Threading;
 
     public class UserInputInfo {
         [DllImport("user32.dll", SetLastError = true)]
@@ -18,75 +102,56 @@ Add-Type @"
 
 function Get-IdleDuration {
     $lii = New-Object UserInputInfo+LASTINPUTINFO
-    $lii.cbSize = [System.UInt32][System.Runtime.InteropServices.Marshal]::SizeOf($lii)
+    $lii.cbSize = [System.Runtime.InteropServices.Marshal]::SizeOf($lii)
 
-    if ([System.Boolean][UserInputInfo]::GetLastInputInfo([ref]$lii)) {
-        $idleMillis = [Environment]::TickCount - $lii.dwTime
+    if ([UserInputInfo]::GetLastInputInfo([ref]$lii)) {
+        $idleMillis = [System.Environment]::TickCount - $lii.dwTime
         return $idleMillis / 1000.0
     } else {
-        throw [InvalidOperationException]"Failed to retrieve last input info."
+        throw "Failed to retrieve last input info."
     }
+}
+
+function Is-UserActive {
+    $idleness = Get-IdleDuration
+    return $idleness -lt $passiveThreshold
 }
 
 function Update-ActivityStats {
-    param($idleness, [ref]$activeTime, [ref]$inactiveTime, $interval)
+    param (
+        [double]$idleness
+    )
 
-    if ($idleness -gt 10) {
-        Write-Host "Away ($idleness sec w/o interact)"
-        $inactiveTime.value += $interval
+    $isUserActive = Is-UserActive
+
+    if ($isUserActive) {
+        Write-Host "away or active ($idleness sec w/o interact)"
+        $global:activeTime += $interval
     } else {
-        Write-Host "Active ($idleness sec w/o interact)"
-        $activeTime.value += $interval
+        Write-Host "away or active ($idleness sec w/o interact)"
+        $global:inactiveTime += $interval
     }
-}
-
-function Format-Time {
-    param($seconds)
-
-    $minutes = [math]::floor($seconds / 60)
-    $remainingSeconds = $seconds % 60
-
-    if ($minutes -gt 0) {
-        return "$($minutes) minute(s) $($remainingSeconds) second(s)"
-    } else {
-        return "$($remainingSeconds) second(s)"
-    }
-}
-
-[bool]$consoleAllocated = $false
-
-$activeTime = 0
-$inactiveTime = 0
-$interval = 5
-$totalTime = 0
-$targetDuration = 300
-$logFilePath = Join-Path $env:TEMP "WorkTimepowershell.log"
-
-$sw = [System.IO.File]::AppendText($logFilePath)
-
-if ($sw.BaseStream.Length -eq 0) {
-    $sw.WriteLine("ActiveTime`tInactiveTime`tTimestamp")
+    Write-Host "Is User Active: $isUserActive"
 }
 
 while ($totalTime -lt $targetDuration) {
     Start-Sleep -Seconds $interval
-
     $idleness = Get-IdleDuration
-    Update-ActivityStats $idleness ([ref]$activeTime) ([ref]$inactiveTime) $interval
+    Update-ActivityStats -idleness $idleness
 
-    $totalTime += $interval
-
-    $now = Get-Date
-
-    Write-Host "$(Format-Time $activeTime)`t$(Format-Time $inactiveTime)`t$now"
-
-    if ($totalTime % $targetDuration -eq 0) {
-        Write-Host "Total Active Time: $(Format-Time $activeTime)"
-        Write-Host "Total Inactive Time: $(Format-Time $inactiveTime)"
-
-        $formattedData = "$(Format-Time $activeTime)`t$(Format-Time $inactiveTime)`t$($now.ToString('dd.MM.yyyy HH:mm:ss'))"
-        $sw.WriteLine($formattedData)
-    }
+    $global:totalTime += $interval
 }
 
-$sw.Dispose()
+$activeMinutes = [math]::Round($global:activeTime / 60, 2)
+$inactiveMinutes = [math]::Round($global:inactiveTime / 60, 2)
+
+Write-Host "Total Active Time: $($activeMinutes) minutes"
+Write-Host "Total Inactive Time: $($inactiveMinutes) minutes"
+#Wmi 'e verileri kayıt eder
+Set-WmiInstance -Namespace root\cimv2 -Class $NewClassName -Argument @{
+    ActiveTime = $activeMinutes
+    InactiveTime = $inactiveMinutes
+    ScriptLastRan = $Date
+} 
+
+Get-WmiObject -Class $NewClassName | Select-Object ActiveTime, InactiveTime, Timestamp, ScriptLastRan | Sort-Object ScriptLastRan | ft
--- README.md
+++ README.md
@@ -1,3 +1,46 @@
 # WorkTimeMonitor
 #Task Schedule Command
 wscript "c:\temp\run_hidden.vbs" "C:\temp\ActivityTracker.ps1"
+
+# PowerShell İzleyici ve Loglayıcı Scripti
+
+Bu PowerShell scripti, bir bilgisayarın kullanıcı etkinliğini izler ve belirli bir süre boyunca aktif ve pasif zamanını hesaplar. Script, kullanıcı etkinliğini izlemek için Windows API'sini kullanır ve bir WMI sınıfı aracılığıyla toplanan verileri kaydeder.
+
+## Ayarlar
+
+- `$NewClassName`: WMI sınıfının adı.
+- `$activeTime`: Aktif zamanı izlemek için kullanılan değişken.
+- `$inactiveTime`: Pasif zamanı izlemek için kullanılan değişken.
+- `$interval`: Kontrol aralığı (saniye cinsinden).
+- `$targetDuration`: Toplam çalışma süresi hedef süresi (saniye cinsinden).
+- `$passiveThreshold`: Pasif olarak kabul edilecek aralık (saniye cinsinden).
+
+## WMI Sınıfının Oluşturulması
+
+- Belirtilen ad ve özelliklere sahip bir WMI sınıfı oluşturur.
+
+## Loglama Fonksiyonu (`Write-Log`)
+
+- Hata, uyarı veya bilgi düzeylerinde loglama yapar.
+- Log dosyasının boyutunu kontrol eder ve gerektiğinde yeniden oluşturur.
+- Log dosyasına formatlanmış mesajı ekler.
+
+## Kullanıcı Etkinliğinin İzlenmesi
+
+- `UserInputInfo` sınıfı aracılığıyla kullanıcı etkinliğini izler.
+- Kullanıcının etkin olup olmadığını belirler.
+- Kullanıcının etkin olduğu süreyi ve pasif olduğu süreyi günceller.
+
+## Ana Döngü
+
+- Belirtilen hedef süreye ulaşılana kadar ana döngüde kalır.
+- Her döngüde kullanıcı etkinliği güncellenir ve toplam süre hesaplanır.
+
+## WMI Sınıfına Veri Gönderme ve Gösterme
+
+- Toplam aktif ve pasif süreleri WMI sınıfına kaydeder.
+- Kaydedilen verileri WMI sınıfından alır ve gösterir.
+
+---
+
+Bu script, bilgisayar kullanım izleme ve raporlama gibi senaryolar için kullanılabilir ve uygun bir şekilde yapılandırılarak işletim sistemi kullanımını izlemek için kullanılabilir.
